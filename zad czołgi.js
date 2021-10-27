4aa01d6a8a7310796c99ca3f9cc1dc1bcbb86794
DawidKwiatkowski740 <79514899+DawidKwiatkowski740@users.noreply.github.com>
2021-10-27 15:44:52 +0200

    Add files via upload

---
+++ zad czołgi.js
@@ -0,0 +1,28 @@
+<html>
+    <head>
+
+    </head>
+        <body>
+
+function czołg(waga,dłLufy,krajPochodzenia,IlZałogi,predkoscMax,amunicja,grPancerza,marka) 
+{
+    
+this.marka=marka;
+this.amunicja=amunicja;
+this.waga=waga;
+this.dłLufy=dłLufy;
+this.krajPochodzenia=krajPochodzenia;
+this.IlZałogi=IlZałogi;
+this.predkoscMax=predkoscMax;
+this.grPancerza=grPancerza;
+}
+
+ciężki = new czołg("")
+lekki = new czołg("")
+średni = new czołg("")
+amerykańskie = new  czołg("")
+niemieckie = new czołg("x",y,z,)
+
+        </body>
+   
+</html>
